118d46ded5129de33b25fa9ffc565915347a063f
Anna Samsonenko <foster.anna93@gmail.com>
2022-06-03 01:03:29 +0300

    clear comments

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -25,21 +25,3 @@ router.put("/:id", updateContact);
 router.patch("/:id", validateRequest(schemaPatch), updateStatusContact);
 
 module.exports = router;
-
-// const express = require("express");
-// const {
-// listContacts, getById, addContact, removeContact, updateContact
-// } = require("../../controllers/contacts");
-// const router = express.Router();
-// const { schemaPatch, schemaCreate } = require("../../models/product");
-// const { validateRequest } = require("../../middlewares/validateRequest");
-
-// router.get("/", getAll);
-// // TODO: add validation for id using mongoose function isValidObjectId (for sending correct status)
-// router.get("/:id", getById);
-// router.post("/", validateRequest(schemaCreate), create);
-// router.put("/:id", updateById);
-// router.patch("/:id/available", validateRequest(schemaPatch), updateAvailability);
-// router.delete("/:id", deleteById);
-
-// module.exports = router;
